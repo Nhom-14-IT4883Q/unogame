6f4d926eb51b8f4433a7a963e5b9c1b2a390c412
desertagent <buiduyanh0@gmail.com>
2023-11-23 19:40:38 +0700

    add server and connetion

---
+++ src/components/Game.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Game = () => {
+    const data = queryString.parse(props.location.search)
+
+    //initialize socket state
+    const [room, setRoom] = useState(data.roomCode)
+    const [roomFull, setRoomFull] = useState(false)
+    const [users, setUsers] = useState([])
+    const [currentUser, setCurrentUser] = useState('')
+    const [message, setMessage] = useState('')
+    const [messages, setMessages] = useState([])
+
+    useEffect(() => {
+        const connectionOptions =  {
+            "forceNew" : true,
+            "reconnectionAttempts": "Infinity", 
+            "timeout" : 10000,                  
+            "transports" : ["websocket"]
+        }
+        socket = io.connect(ENDPOINT, connectionOptions)
+
+        socket.emit('join', {room: room}, (error) => {
+            if(error)
+                setRoomFull(true)
+        })
+
+        //cleanup on component unmount
+        return function cleanup() {
+            socket.emit('disconnect')
+            //shut down connnection instance
+            socket.off()
+        }
+    }, [])
+
+    
+  return (
+    <div>Game</div>
+  )
+}
+
+export default Game
